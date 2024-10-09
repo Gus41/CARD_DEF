9f8c7e1d229e247274ad6c1db81c79325213110c
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-10-09 15:31:54 -0300

    Update Card.tsx

--- card/src/components/Card.tsx
+++ card/src/components/Card.tsx
@@ -10,7 +10,7 @@ export default function Card(props: cardProps) {
         <div className="flex flex-col max-w-[340px] shadow-lg overflow-hidden" id="card">
             <div className="bg-[#0052ea] pt-4 pl-4 pb-4 pr-4 relative min-h-[125px]">
             <h2 className="relative z-20 text-white">{props.title}</h2>
-            <Image className="absolute top-0 right-0 " src={'/topo.png'} width={46} height={60} alt="Element" />
+            <Image className="absolute top-0 right-0 " src={'/topo.png'} width={46} height={60} alt="Element" quality={100} />
         </div>
 
             <div className="bg-gray-300 relative flex items-center justify-center">
@@ -32,7 +32,7 @@ export default function Card(props: cardProps) {
             </div>
 
             <div className="flex items-center justify-center p-2 bg-[#0034a4]">
-                <Image alt="logo" src={'/ucs.png'} width={70} height={70} />
+                <Image alt="logo" src={'/ucs.png'} width={70} height={70} quality={100} />
             </div>
         </div>
     );
