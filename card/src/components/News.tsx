a4406af821c9d117bc3838c3397cd8d023a7c677
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-10-09 14:50:27 -0300

    Update News.tsx

--- card/src/components/News.tsx
+++ card/src/components/News.tsx
@@ -1,42 +1,49 @@
 import { useEffect, useState } from 'react';
 
-interface newsProps{
-    set: (title:string,description:string,image_path:string)=>void
+interface newsProps {
+  set: (title: string, description: string, image_path: string) => void;
 }
 
-interface Descricao{
-    descricao:string
+interface Descricao {
+  descricao: string;
 }
-interface Featured_image{
-    guid: string
+interface Featured_image {
+  guid: string;
 }
 interface NewsItem {
   id: number;
-  title: string,
-  descricao:Descricao,
-  featured_image : Featured_image
+  title: string;
+  descricao: Descricao;
+  featured_image: Featured_image;
 }
 
-const NewsComponent = (props : newsProps) => {
+const NewsComponent = (props: newsProps) => {
   const [news, setNews] = useState<NewsItem[]>([]);
 
   useEffect(() => {
-    fetch('https://sou.ucs.br/noticias/wp-json/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        setNews(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching news:', error);
-      });
+    const apiUrl = process.env.NEXT_PUBLIC_NEWS_API_URL; 
+    if (apiUrl) {
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          setNews(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching news:', error);
+        });
+    }
   }, []);
 
   function renderNews() {
     return news.map((n, i) => {
       return (
-        <p onClick={()=>{
-            props.set(n.title,n.descricao.descricao,n.featured_image.guid)
-        }} className="bg-gray-800 m-2 p-2 rounded-lg cursor-pointer" key={i}>
+        <p
+          onClick={() => {
+            props.set(n.title, n.descricao.descricao, n.featured_image.guid);
+          }}
+          className="bg-gray-800 m-2 p-2 rounded-lg cursor-pointer"
+          key={i}
+        >
           {n.title}
         </p>
       );
@@ -45,7 +52,7 @@ const NewsComponent = (props : newsProps) => {
 
   return (
     <div className="absolute right-0 top-0 w-64 h-[500px] overflow-y-scroll bg-black shadow-lg rounded-lg p-4">
-      <h1 className="text-lg font-bold mb-4">Ultimas notícias</h1>
+      <h1 className="text-lg font-bold mb-4">Últimas notícias</h1>
       {news.length > 0 ? renderNews() : <p>Loading news...</p>}
     </div>
   );
